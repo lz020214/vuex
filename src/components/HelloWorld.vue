af979d9c260646b644c582fecc89f37e8150476b
李政 <you@example.com>
2024-05-05 03:19:40 +0800

    更加简便的方式

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,15 +1,29 @@
 <template>
 <div>
   <!-- 调用仓库里面的状态数据count -->
-  {{ this.$store.state.count }}
+  <!-- {{ this.$store.state.count }} -->
+  {{ count }}
   <button @click="add">+1</button>
 </div>
 
 </template>
 
 <script>
+import { mapState } from 'vuex'
 export default {
   name: 'HelloWorld',
+  // 通过计算属性直接获得值，不必写的过于麻烦
+  // computed:{
+  //   count(){
+  //     return this.$store.state.count
+  //   }
+  // },
+
+  // 更加简便的方法
+  computed:mapState([
+    'count',
+  ]),
+
   methods:{
     add(){
       // 通过提交到仓库里面进行+1操作
